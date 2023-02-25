bab06c998db2d02eec0fb9acf31cc154caf2aa71
razevedomil <117126406+razevedomil@users.noreply.github.com>
2023-02-24 22:11:46 -0300

    Introduzindo consumo de api

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -2,12 +2,14 @@ import "../parallax/Parallax.css"
 import Banner from "./Banner"
 import Formulario from "../formulario/Formulario"
 import Footer from "../footer/Footer"
+import Home from "../api/Home"
 
 const MyParallax = () => {
     return(
         <div className="wrapper">
         <main>
             <Formulario />
+            <Home />
             
         </main>
         
