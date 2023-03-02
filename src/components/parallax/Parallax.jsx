2eacf04cfc59175e1a8a874fba48c2abe7bd5fec
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-03-02 17:18:17 -0300

    iniciando pág sobre

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,10 +1,12 @@
 import "../parallax/Parallax.css"
 import Formulario from "../formulario/Formulario"
 import Api from "../api/Api"
+import Sobre from "../sobre/Sobre"
 
 const MyParallax = () => {
     return(
         <div className="wrapper">
+            <Sobre />
             <Formulario />
             <Api />
     </div>
