e6c56a7b22d2deedbdbbeca07ad105efe745a966
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-03-07 16:19:58 -0300

    Add files via upload

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,11 +1,31 @@
+import "../pages/Galeria.css"; // importando arquivo de estilos
 
 function Galeria() {
     return (
         <div>
-            <h1>Galeria de tatuagens</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere saepe in a non id! Animi voluptate consequuntur quos, aliquam ullam perspiciatis distinctio quasi corrupti, nostrum excepturi autem ipsa quisquam inventore!</p>
+            <h1 className="galeriah1">Galeria de Tatuagens</h1>
+            <div className="gallery">
+                <img src="https://images.unsplash.com/photo-1607382007937-fe3a9d196b7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Tatuagem 1" />
+                <img src="https://images.unsplash.com/photo-1571989545237-7727887541ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Tatuagem 2" />
+                <img src="https://images.unsplash.com/photo-1525135850648-b42365991054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Tatuagem 3" />
+                <img src="https://images.unsplash.com/photo-1602103494381-5db708061ba6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=418&q=80" alt="Tatuagem 4" />
+                <img src="https://images.unsplash.com/photo-1619009542298-584066f6fd30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="Tatuagem 5" />
+                <img src="https://images.unsplash.com/flagged/photo-1575495407752-bfb6fb0518bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=529&q=80" alt="Tatuagem 7" />
+                <img src="https://images.unsplash.com/photo-1611501275019-9b5cda994e8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Tatuagem 8" />
+                <img src="https://images.unsplash.com/photo-1562379825-415aea84ebcf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="Tatuagem 9" />
+                <img src="https://images.unsplash.com/photo-1598371839696-5c5bb00bdc28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=447&q=80" alt="Tatuagem 10" />
+                <img src="https://images.unsplash.com/photo-1586243287039-23f4c8e2e7ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Tatuagem 11" />
+                <img src="https://images.unsplash.com/photo-1540174053853-1cc5d1fa8814?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80" alt="Tatuagem 12" />
+                <img src="https://images.unsplash.com/photo-1597852075234-fd721ac361d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Tatuagem 13" />
+                <img src="https://images.unsplash.com/photo-1454329030972-00583f5f051f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1161&q=80" alt="Tatuagem 14" />
+                <img src="https://images.unsplash.com/flagged/photo-1575495407752-bfb6fb0518bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=529&q=80%22" alt="Tatuagem 15" />
+                <img src="https://images.unsplash.com/photo-1482328177731-274399da39f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Tatuagem 16" />
+            </div>
         </div>
-    )
+
+
+
+    );
 }
 
-export default Galeria
+export default Galeria;
