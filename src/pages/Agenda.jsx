cdc7b82ffedfacb84ae23d80ae833da29952965a
razevedomil <117126406+razevedomil@users.noreply.github.com>
2023-03-10 14:54:12 -0300

    att agenda

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -6,6 +6,27 @@ import { Link } from "react-router-dom";
 
 import "./Agenda.css";
 
+function Formulario() {
+    const [opcaoSelecionada, setOpcaoSelecionada] = useState('');
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      // código para enviar os dados para a API
+      const response = await fetch('sua-api.com/endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ opcaoSelecionada })
+      });
+  
+      // código para lidar com a resposta da API
+      const data = await response.json();
+      console.log(data);
+    }
+
+}
 
 const Agenda = () => {
     const [Agenda, setAgenda] = useState([])
@@ -30,20 +51,56 @@ const Agenda = () => {
 
     return (
 
-        <div className="Agenda">
+        <div className="Agenda1">
             <h1 className="Agenda">Agenda:</h1>
             <div className="Agenda">
             {(Agenda.map((Agend) => (
                 <div className="Agend" key={Agend.id}>
-                    <img src={Agend.dia}/>
-                    <h2 className="Agend-nome">{Agend.Dia}</h2>
-                    <p className="Agend-sobre">{Agend.Horário} - {Agend.Tatuador} - {Agend.Estilo} </p>
+                    <h2 className="Agend-Dia">{Agend.Dia}</h2>
+                    <p className="Agend-Status">{Agend.Status}</p>
+                    <p className="Agend-Horario">{Agend.Horário}</p>
+                    <p className="Agend-Tatuador">{Agend.Tatuador}</p>
                 </div>
-
             ))
         )}
         </div>
-         </div>
+        <div className="Marcar-Horario">
+        <h1 className="Horario">Marcar Horário:</h1>
+        <div className="form2">
+        <form id="myForm" onsubmit="return validarFormulario()">
+                    <label for="opcoes">Dia: </label>
+                    <select id="opcoes" name="opcoes" required className='input'>
+                        <option value="" hidden>Escolha um dia</option>
+                        <option>Segunda </option>
+                        <option>Terça </option>
+                        <option>Quarta </option>
+                        <option>Quinta </option>
+                        <option>Sexta </option>
+                    </select>
+                    <label>Horário: </label>
+                    <select required className='input'>
+                        <option value="" hidden>Escolha um horário</option>
+                        <option>Manhã </option>
+                        <option>Tarde </option>
+                        <option>Noite </option>
+                    </select>
+                    <label>Tatuador: </label>
+                    <select required className='input'>
+                        <option value="" hidden>Escolha um tatuador</option>
+                        <option>Lívia (Blackwork)</option>
+                        <option>Peter (Realismo)</option>
+                        <option>Bárbara (Pontilhismo)</option>
+                        <option>Gabriel (Old School)</option>
+                        <option>Laura (Lettering)</option>
+                        <option>Sávio (Fine Line)</option>
+                        <option>Escolha por mim!</option>
+                    </select>
+                    <input name="marcar" type="submit" value="Enviar" />
+                </form>
+        </div>
+        </div>
+        </div>
+         
     );
 
 };
